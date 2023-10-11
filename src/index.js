d174dcd81c35e997606f78efce2179458058fefb
mauZzi <142857889+mauZzii@users.noreply.github.com>
2023-10-10 20:44:30 -0500

    handling of toml, yaml, and json files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './style.css';
 import Icon from './header.svg';
 import Data from './data.xml';
 import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+//output TOML Example and Tom Preston-Werner to console
+console.log(toml.title);
+console.log(toml.owner.name);
+
+//output YAML Example and Tom Preston-Werner to console
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+//output JSON Example and Tom Preston-Werner to console
+console.log(json.title);
+console.log(json.owner.name);
 
 function component() {
     const element = document.createElement('div');
