fd60320d8ab57e168a35bc153e38e54a96fd901e
mauZzi <142857889+mauZzii@users.noreply.github.com>
2023-10-10 19:52:49 -0500

    imported styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import _ from 'lodash';
+import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
-
-document.body.appendChild(component());
-
-import myName from './myName';
 
 function component2() {
     const element = document.createElement('div');
@@ -19,4 +18,5 @@ function component2() {
     return element;
 }
 
+document.body.appendChild(component());
 document.body.appendChild(component2());
