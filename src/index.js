ec6be5bbcd29eb1c62330c684cbde3639325f634
mauZzi <142857889+mauZzii@users.noreply.github.com>
2023-10-10 21:35:23 -0500

    handling output management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.textContent = _.join(['Hello', 'webpack'], ' ');
+
+    btn.textContent = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
